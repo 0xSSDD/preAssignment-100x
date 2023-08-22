9339a099bc734b275cc4fefa105fbdeb2036d9fe
SSDD <0xSSDD@protonmail.com>
2023-08-22 10:57:06 +0300

    adds another function

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+// overload the console.log function
+console.log = (message) => {
+  // do something with the message
+  console.log(message);
+};
